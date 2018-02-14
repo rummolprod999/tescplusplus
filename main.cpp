dfdec36098e48e267753944e304074fea1f28b45
rummolprod999 <rummolprod999@gmail.com>
2018-02-14 12:38:00 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
-#include <string>
-#include <utility>
-
-using std::string;
-using std::cout;
-using std::endl;
-
-class Person
-{
-public:
-    string name;
-    int age;
-    Person(string n, int a)
-    {
-        name = std::move(n); age = a;
-    }
-    void move()
-    {
-        cout << name << " is moving" << endl;
-    }
-};
+double divide(int, int);
 int main()
 {
-    Person person = Person("Tom", 22);
-    cout << "Name: " << person.name << "\tAge: " << person.age << endl;
-    person.name = "Bob";
-    person.move();
-
+    int x = 500;
+    int y = 0;
+    try
+    {
+        double z = divide(x, y);
+        std::cout << z << std::endl;
+    }
+    catch (const char* msg)
+    {
+        std::cout << msg << std::endl;
+    }
+    std::cout << "The End..." << std::endl;
     return 0;
+}
+
+double divide(int a, int b)
+{
+    if (b == 0)
+        throw "Division by zero!";
+    return a / b;
 }
