50b25c19d861d55f5159004a7255fd49b2214857
rummolprod999 <rummolprod999@gmail.com>
2019-09-06 11:04:49 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 typedef char CHAR, *PSTR;
 struct A
 {
@@ -8,8 +9,11 @@ struct A
 
 int main()
 {
-    A a;
-    PSTR x;
-    const char* pX, Y, Z; //1
+    int i = 0, &r = i, t = 5;
+    const int ci = i, &cr = ci;
+    auto e = &ci;
+    e = &t;
+    int *const pp = &t;
+    cout << typeid(e).name() << endl;
     return 0;
 }
